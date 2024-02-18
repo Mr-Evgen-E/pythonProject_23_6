b3fa65d976e6a4fb90e68b22b1f9902c085c4e80
Mr-Evgen-E <you@example.comeev-evgeny@yandex.ru>
2024-02-18 18:30:42 +0300

    continue 2

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 TOKEN = '6832851527:AAG5o94cSvqamGyd4BwHvtUYmKYxoL37N7w'
+
+keys = {
+    "рубль": "RUB",
+    "доллар": "USD",
+    "евро": "EUR"
+}
--- main.py
+++ main.py
@@ -1,26 +1,22 @@
-import requests
 import telebot
-import json
-from config import TOKEN
+from extensions import ConvertionExceptions, CurrencyConverter
+from config import TOKEN, keys
 from help import text_help
 
 bot = telebot.TeleBot(TOKEN)
 
-keys = {
-    "рубль": "RUB",
-    "доллар": "USD",
-    "евро": "EUR"
-}
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message: telebot.types.Message):
     bot.reply_to(message, f"Здравствуйте, {message.chat.username} \n\
-Я предоставляю Вам информацию о валюте.\nА так же могу сконвертировать ее.\n"
-+ f"{text_help}")
+Я предоставляю Вам информацию о валюте.\nА также могу конвертировать ее.\n"
+                 + f"{text_help}")
+
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message: telebot.types.Message):
     bot.reply_to(message, text_help)
+
 
 @bot.message_handler(commands=['values'])
 def values(message: telebot.types.Message):
@@ -29,12 +25,24 @@ def values(message: telebot.types.Message):
         text = "\n".join((text, key))
     bot.reply_to(message, text)
 
+
 @bot.message_handler(content_types=['text'])
 def converter(message: telebot.types.Message):
-    quote, base, amount = message.text.split(" ")
-    r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={keys[quote]}&tsyms={keys[base]}")
-    text = json.loads(r.content)[keys[base]]
-    bot.send_message(message, text)
+    try:
+        values_ = message.text.split(' ')
+
+        if len(values_) != 3:
+            raise ConvertionExceptions("Недостаточно или много параметров")
+
+        quote, base, amount = values_
+        total_base = CurrencyConverter.get_price(quote, base, amount)
+    except ConvertionExceptions as e:
+        bot.reply_to(message, f"Ошибка пользователя.\n{e}")
+    except Exception as e:
+        bot.reply_to(message, f"Не удалось обработать команду\n{e}")
+    else:
+        text = f"Цена: {amount} {quote} в {base} - {total_base}"
+        bot.send_message(message.chat.id, text)
 
 
 @bot.message_handler(content_types=['text', 'document', 'audio', 'voice'])
